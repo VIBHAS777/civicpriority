9361bfade71ad2dae81d4b7615ad25a6c1c9485e
VIBHAS777 <vibhasf17@gmail.com>
2026-05-20 23:16:04 +0530

    Align Android App with React Website by adding Firebase Firestore integration

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -79,6 +79,10 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
 
     // Testing
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-firestore")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
